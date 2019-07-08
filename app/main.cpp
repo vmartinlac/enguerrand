9caeaeb72b0544a06197a4f01d61dc2a0507ca3d
Victor MARTIN-LAC <victormartinlac@gmail.com>
2019-07-09 00:11:03 +0200

    some work

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,93 @@
 #include <tbb/flow_graph.h>
 #include <tbb/scalable_allocator.h>
+#include <memory>
 #include <iostream>
 #include "FileVideoSource.h"
 #include "EdgeCirclesData.h"
+
+struct SLAMFrameCircle
+{
+    cv::Vec3f circle;
+    size_t previous;
+    bool has_previous;
+};
 
 class SLAMFrame
 {
 public:
 
     VideoFrame video_frame;
-    cv::Mat3b input_image;
-    EdgeCirclesData edge_circles_data;
+    cv::Mat2f normals;
+    cv::Mat1b flags;
+    std::vector<SLAMFrameCircle> circles;
 };
 
 typedef std::shared_ptr<SLAMFrame> SLAMFramePtr;
+
+class VideoNodeBody
+{
+public:
+
+    VideoNodeBody(VideoSourcePtr video)
+    {
+        mVideo = std::move(video);
+    }
+
+    VideoNodeBody(const VideoNodeBody& o)
+    {
+        mVideo = o.mVideo;
+    }
+
+    bool operator()(SLAMFramePtr& frame)
+    {
+        VideoFrame video_frame;
+        mVideo->read(video_frame);
+
+        if( video_frame.isValid() )
+        {
+            frame = std::allocate_shared<SLAMFrame,tbb::scalable_allocator<SLAMFrame>>(tbb::scalable_allocator<SLAMFrame>());
+            frame->video_frame = std::move(video_frame);
+            mVideo->trigger();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+protected:
+
+    VideoSourcePtr mVideo;
+};
+
+class EdgeNodeBody
+{
+public:
+
+    EdgeNodeBody()
+    {
+    }
+
+    SLAMFramePtr operator()(SLAMFramePtr frame)
+    {
+        return frame;
+    }
+};
+
+class CirclesNodeBody
+{
+public:
+
+    CirclesNodeBody()
+    {
+    }
+
+    SLAMFramePtr operator()(SLAMFramePtr frame)
+    {
+        return frame;
+    }
+};
 
 int main(int num_args, char** args)
 {
@@ -27,42 +101,21 @@ int main(int num_args, char** args)
     video_source->setFileName("/home/victor/Développement/enguerrand/data/VID_20190615_150222.mp4");
     */
     video_source->setFileName("/home/victor/Développement/enguerrand/data/VID_20190615_180529.mp4");
-    video_source->setFileName("/home/victor/Développement/enguerrand/data/vid.mkv");
 
     if( video_source->open() == false ) exit(1);
 
     video_source->trigger();
 
-    auto video_node_proc = [&video_source] (SLAMFramePtr& output) -> bool
-    {
-        video_source->read(output->video_frame);
-        if( output->video_frame.isValid() )
-        {
-            video_source->trigger();
-            output->input_image = output->video_frame.getView(0);
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    };
+    tbb::flow::source_node<SLAMFramePtr> video_node(g, VideoNodeBody(video_source));
+    tbb::flow::function_node<SLAMFramePtr,SLAMFramePtr> edge_node(g, 1, EdgeNodeBody());
+    tbb::flow::function_node<SLAMFramePtr,SLAMFramePtr> circles_node(g, 1, CirclesNodeBody());
 
-    tbb::flow::source_node<SLAMFramePtr> video_node(g, video_node_proc);
-
-    /*
-
-    auto process_node_proc = [] (const VideoFrame& f) -> tbb::flow::continue_msg
-    {
-        return tbb::flow::continue_msg();
-    };
-
-    tbb::flow::function_node<VideoFrame,tbb::flow::continue_msg> process_node(g, 1, process_node_proc);
-
-    tbb::flow::make_edge(video_node, process_node);
+    make_edge(video_node, edge_node);
+    make_edge(edge_node, circles_node);
 
     g.wait_for_all();
-    */
+
+    video_source->close();
 
     return 0;
 }
